4e2636c892b5895313fafaa0638ca2ccb6328d07
kkmetech <kkmetech@gmail.com>
2025-02-10 11:18:57 +0700

    add Radix UI dialog component and integrate Cloudflare Turnstile for human verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { Button } from "@/components/ui/button"
+import React from 'react';
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
 
 function App() {
+  const [open, setOpen] = React.useState(false);
   return (
-    <div>
-    <Button>Click me</Button>
-  </div>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="default">Click me</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Verify that you are human</DialogTitle>
+        </DialogHeader>
+        <div className="w-full flex justify-center p-4">
+          {/* Cloudflare Turnstile widget */}
+          <div
+            className="cf-turnstile"
+            data-sitekey="0x4AAAAAAA7SN9BgICU6k8R5"
+            data-callback="handleCallback"
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
 
